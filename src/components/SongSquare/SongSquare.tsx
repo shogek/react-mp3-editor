ae770cd80261376a990ac07ec99057eea6fbe20f
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2019-06-30 18:48:47 +0300

    SongSquare now has a function component

--- src/components/SongSquare/SongSquare.tsx
+++ src/components/SongSquare/SongSquare.tsx
@@ -1,56 +1,68 @@
-import React, { Component } from "react";
-// import Song from "../../models/song";
-import TagReader from "jsmediatags";
-import TagWriter from "browser-id3-writer";
-import FileSaver from "file-saver";
+import React from 'react';
+import Song from '../../models/song';
+import DefaultCover from './cover_350x350.png';
 
 type Props = {
-    songSelectedEvent: Event
-};
-type State = {};
+    song: Song;
+    onClickClose: Function;
+}
 
-class SongSquare extends Component<Props, State> {
-    constructor(props) {
-        super(props);
+function SongSquare(props: Props) {
+    const { song, onClickClose } = props;
 
-        this.parseEvent(props.songSelectedEvent);
-    }
+    return (
+        <div className='row mzt-row-song align-items-center'>
+            <div className='col-auto'>
+                <img
+                    className='img-thumbnail'
+                    alt='album cover'
+                    src={song.albumCover ? song.albumCover.dataAsTagSrc : DefaultCover} />
+            </div>
 
-    parseEvent(e: Event) {
-        // const reader = new FileReader();
-        // reader.onload = function (a) {
-        //     const arrayBuffer = reader.result;
-        //     const writer = new TagWriter(arrayBuffer);
-        //     writer.setFrame("TYER", 2000);
-        //     writer.addTag();
-        //     // FileSaver.saveAs(writer.getBlob(), "modified.mp3");
-        // };
-        // reader.onprogress = function (e) {
-        //     console.log(Math.floor((e.loaded / e.total) * 100));
-        // }
-        // reader.onerror = function (e) {
-        //     debugger;
-        //     console.error("Reader error", reader.error);
-        // };
-        // reader.readAsArrayBuffer(e.target.files[0]);
-    }
+            <div className='col mzt-song-text'>
+                <div className='row'>
+                    <div className='col'>
+                        <h3>{song.title}</h3>
+                    </div>
+                </div>
 
-    render() {
-        // const { song } = this.props;
-        return (
-            // <div>
-            //     <h1>Artist: {song.Artist}</h1>
-            //     <h2>Artist: {song.Title}</h2>
-            //     <h3>Album: {song.Album}</h3>
-            //     <h4>Year: {song.Year}</h4>
-            // </div>
-            <div className='row'>
-                <div className='col-4'>
-                    Text
+                <div className='row'>
+                    <div className='col'>
+                        <h4>{song.artist}</h4>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='col'>
+                        <h4>{song.album}</h4>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='col'>
+                        <h4>{song.year}</h4>
+                    </div>
                 </div>
             </div>
-        );
-    }
+
+            <div className='col-1'>
+                {/* Remove song from list */}
+                <div className='row' title='Remove song from list' onClick={() => onClickClose()}>
+                    <i className="fas fa-times mzt-btn-actions"></i>
+                </div>
+
+                {/* Edit song */}
+                <div className='row' title='Edit song'>
+                    <i className="fas fa-pencil-alt mzt-btn-actions"></i>
+                </div>
+
+                {/* Download song */}
+                <div className='row' title='Download song'>
+                    <i className="fas fa-download mzt-btn-actions"></i>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default SongSquare;
--- src/components/SongSquare/SongSquareContainer.tsx
+++ src/components/SongSquare/SongSquareContainer.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TagReader from 'jsmediatags';
 import Song from '../../models/song';
 import AlbumCover from "../../models/albumCover";
-import DefaultCover from './cover_350x350.png';
+import SongSquare from "./SongSquare";
 import "./song-square.css";
 
 type Props = {
@@ -64,62 +64,7 @@ class SongSquareContainer extends Component<Props, State> {
 
     render() {
         const { song } = this.state;
-        if (!song)
-            return null;
-
-        return (
-            <div className='row mzt-row-song align-items-center'>
-                <div className='col-auto'>
-                    <img
-                        className='img-thumbnail'
-                        alt='album cover'
-                        src={song.albumCover ? song.albumCover.dataAsTagSrc : DefaultCover} />
-                </div>
-
-                <div className='col mzt-song-text'>
-                    <div className='row'>
-                        <div className='col'>
-                            <h3>{song.title}</h3>
-                        </div>
-                    </div>
-
-                    <div className='row'>
-                        <div className='col'>
-                            <h4>{song.artist}</h4>
-                        </div>
-                    </div>
-
-                    <div className='row'>
-                        <div className='col'>
-                            <h4>{song.album}</h4>
-                        </div>
-                    </div>
-
-                    <div className='row'>
-                        <div className='col'>
-                            <h4>{song.year}</h4>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='col-1'>
-                    {/* Remove song from list */}
-                    <div className='row' title='Remove song from list' onClick={this.onCloseClick}>
-                        <i className="fas fa-times mzt-btn-actions"></i>
-                    </div>
-
-                    {/* Edit song */}
-                    <div className='row' title='Edit song'>
-                        <i className="fas fa-pencil-alt mzt-btn-actions"></i>
-                    </div>
-
-                    {/* Download song */}
-                    <div className='row' title='Download song'>
-                        <i className="fas fa-download mzt-btn-actions"></i>
-                    </div>
-                </div>
-            </div>
-        );
+        return song ? (<SongSquare song={song} onClickClose={this.onCloseClick} />) : null;
     }
 }
 
