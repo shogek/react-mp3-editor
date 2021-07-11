41a82b0ebd17685418c10b817b9bb12cff91840a
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2021-07-11 11:54:36 +0300

    Updates

---
+++ precache-manifest.994b74a40343caf06d544ca11d1efa2e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d457402d668671146bc965cac867f35",
+    "url": "/react-mp3-editor/index.html"
+  },
+  {
+    "revision": "6fada156fd833ec87bad",
+    "url": "/react-mp3-editor/static/css/2.533d620f.chunk.css"
+  },
+  {
+    "revision": "5ed92fa25a9c9c12be39",
+    "url": "/react-mp3-editor/static/css/main.2fe3ee91.chunk.css"
+  },
+  {
+    "revision": "6fada156fd833ec87bad",
+    "url": "/react-mp3-editor/static/js/2.d9bf0946.chunk.js"
+  },
+  {
+    "revision": "5ed92fa25a9c9c12be39",
+    "url": "/react-mp3-editor/static/js/main.cd229650.chunk.js"
+  },
+  {
+    "revision": "37d8894cd77200ee141f",
+    "url": "/react-mp3-editor/static/js/runtime~main.e4833951.js"
+  },
+  {
+    "revision": "c4a0bf2b0341f7d0381348f8eeb99f4c",
+    "url": "/react-mp3-editor/static/media/cover_350x350.c4a0bf2b.png"
+  },
+  {
+    "revision": "b21c4c330d28ae0e1e66b4f92179bbaa",
+    "url": "/react-mp3-editor/static/media/song1.b21c4c33.mp3"
+  },
+  {
+    "revision": "7e259364e176c7d439c0bf9d2da3a370",
+    "url": "/react-mp3-editor/static/media/song2.7e259364.mp3"
+  },
+  {
+    "revision": "a2ee4787c335ff6c9323a6646810236b",
+    "url": "/react-mp3-editor/static/media/song3.a2ee4787.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f56a3237c8a95bb99f7ff0834477b314.js"
+  "/react-mp3-editor/precache-manifest.994b74a40343caf06d544ca11d1efa2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-mp3-editor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
